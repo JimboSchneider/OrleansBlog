1ddf7328a9fd0c114cfb1c0c5a19604d4d0137dd
Jim Schneider <jim_schneider@live.com>
2025-03-11 15:42:42 -0600

    Add project files.

---
+++ FullGonzoDevBlog.Abstractions/IPostGrain.cs
@@ -0,0 +1,11 @@
+﻿using FullGonzoDevBlog.Abstractions.Models;
+
+namespace FullGonzoDevBlog.Abstractions
+{
+	[Alias("FullGonzoDevBlog.Abstractions.IPostGrain")]
+	public interface IPostGrain : IGrainWithIntegerKey
+    {
+		Task<Post> GetPost();
+		Task UpdatePost(Post post);
+	}
+}

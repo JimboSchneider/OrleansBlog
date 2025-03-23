68a1fa527588ae0c555df7ea1a164e57fec7bc4f
Jim Schneder <Jim_Schneider@live.com>
2025-03-23 13:54:50 -0600

    feat: renamed the code base (#18)

---
+++ OrleansBlog.Abstractions/IPostGrain.cs
@@ -0,0 +1,11 @@
+﻿using OrleansBlog.Abstractions.Models;
+
+namespace OrleansBlog.Abstractions
+{
+	[Alias("OrleansBlog.Abstractions.IPostGrain")]
+	public interface IPostGrain : IGrainWithIntegerKey
+    {
+		Task<Post> GetPost();
+		Task UpdatePost(Post post);
+	}
+}
---
+++ OrleansBlog.Grains/PostGrain.cs
@@ -0,0 +1,27 @@
+﻿using OrleansBlog.Abstractions;
+using OrleansBlog.Abstractions.Models;
+
+namespace OrleansBlog.Grains
+{
+	public class PostGrain : IPostGrain
+	{
+		private readonly IGrainFactory _grainFactory;
+		private Post _post = new();
+
+		public PostGrain(IGrainFactory grainFactory)
+		{
+			_grainFactory = grainFactory;
+		}
+
+		public async Task<Post> GetPost()
+		{
+			return await Task.FromResult(_post);
+		}
+
+		public Task UpdatePost(Post post)
+		{
+			_post = post;
+			return Task.CompletedTask;
+		}
+	}
+}

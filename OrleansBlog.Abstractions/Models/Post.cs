28e332d7ce47f47289aa5b39fbf4dfb0273b8494
Jim Schneider <jim_schneider@live.com>
2025-03-23 13:36:28 -0600

    feat: renamed the code base

---
+++ OrleansBlog.Abstractions/Models/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrleansBlog.Abstractions.Models
+{
+	[GenerateSerializer, Immutable]
+	[Alias("OrleansBlog.Abstractions.Models.Post")]
+	public record Post
+    {
+		[Id(0)]
+		public int Id { get; set; }
+		[Id(1)]
+		public string Title { get; set; }
+		[Id(2)]
+		public string Content { get; set; }
+		[Id(3)]
+		public DateTime Created { get; set; }
+		[Id(4)]
+		public DateTime? Updated { get; set; }
+		[Id(5)]
+		public string AuthorId { get; set; }
+		[Id(6)]
+		public string[] Tags { get; set; }
+	}
+}

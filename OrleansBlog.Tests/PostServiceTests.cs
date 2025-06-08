4cf16d30f9e0ca8859c30e3bdc93a9a194fa0a3f
Jim Schneder <Jim_Schneider@live.com>
2025-06-07 20:24:52 -0600

    fixing build errors that I didn't check for

--- OrleansBlog.Tests/PostServiceTests.cs
+++ OrleansBlog.Tests/PostServiceTests.cs
@@ -105,7 +105,10 @@ public class PostServiceTests
             Id = 5,
             Title = "Updated Post",
             Content = "Updated Content",
-            AuthorId = "user789"
+            AuthorId = "user789",
+            Tags =
+            [
+            ]
         };
 
         mockGrain.Setup(g => g.UpdatePost(It.IsAny<OrleansBlog.Abstractions.Models.Post>()))
@@ -132,8 +135,24 @@ public class PostServiceTests
         var newerDate = DateTime.UtcNow.AddDays(-1);
 
         // Create posts
-        var post1 = new Post { Title = "Post 1", Content = "Content 1", AuthorId = "user1" };
-        var post2 = new Post { Title = "Post 2", Content = "Content 2", AuthorId = "user2" };
+        var post1 = new Post
+        {
+            Title = "Post 1",
+            Content = "Content 1",
+            AuthorId = "user1",
+            Tags =
+            [
+            ]
+        };
+        var post2 = new Post
+        {
+            Title = "Post 2",
+            Content = "Content 2",
+            AuthorId = "user2",
+            Tags =
+            [
+            ]
+        };
 
         // Setup grains for each post
         var mockGrain1 = new Mock<IPostGrain>();

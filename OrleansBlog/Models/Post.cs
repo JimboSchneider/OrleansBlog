6c1de971347408c01810bc4f1a176636e3c9d6fb
Jim Schneder <Jim_Schneider@live.com>
2025-06-07 20:20:23 -0600

    Fixing more warnings.

--- OrleansBlog/Models/Post.cs
+++ OrleansBlog/Models/Post.cs
@@ -4,10 +4,10 @@
 	{
 		public long Id { get; set; }
 		public string Title { get; set; } = string.Empty; // Ensure non-null
-		public string Content { get; set; } = string.Empty; // Ensure non-null
+		public required string Content { get; set; } = string.Empty; // Ensure non-null
 		public DateTime Created { get; set; }
 		public DateTime? Updated { get; set; }
-		public string AuthorId { get; set; } = string.Empty; // Ensure non-null
-		public string[] Tags { get; set; } = []; // Ensure non-null
+		public required string AuthorId { get; set; } = string.Empty; // Ensure non-null
+		public required string[] Tags { get; set; } = []; // Ensure non-null
 	}
 }
